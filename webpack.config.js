823387fb89af17e29bd2b90994e48ccc24940bf5
jess2 <jess2.developer@gmail.com>
2020-11-14 12:21:19 +0900

    코드 정리

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
         test: /\.css$/,
         use: [
           'vue-style-loader',
-          'style-loader',
+          'css-loader',
           'style-loader'
         ],
       }, {
